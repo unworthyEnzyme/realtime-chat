4cbaf68260bf37de625e14ccac020a61d70bb1e7
unworthyEnzyme <unworthy_enzyme@protonmail.com>
2022-08-06 20:47:28 +0300

    go like error handling

--- apps/server/src/auth/auth.router.ts
+++ apps/server/src/auth/auth.router.ts
@@ -2,6 +2,15 @@ import express from "express";
 import { z } from "zod";
 import prisma from "../prisma";
 
+type Result<T, E extends Error = Error> = [T, null] | [null, E];
+const safe = async <T>(callback: () => Promise<T>): Promise<Result<T>> => {
+	try {
+		return [await callback(), null];
+	} catch (err) {
+		return [null, err as Error];
+	}
+};
+
 const router = express.Router();
 
 const signupSchema = z.object({
@@ -10,8 +19,16 @@ const signupSchema = z.object({
 	password: z.string(),
 });
 router.post("/signup", async (req, res) => {
-	const data = signupSchema.parse(req.body);
-	const user = await prisma.user.create({ data });
+	const [data, err] = await safe(() => signupSchema.parseAsync(req.body));
+	if (err !== null) {
+		res.status(400).json({ error: "Invalid schema" });
+		return;
+	}
+	const [user, err2] = await safe(() => prisma.user.create({ data }));
+	if (err2 !== null) {
+		res.status(400).json({ error: "The user already exists" });
+		return;
+	}
 	res.json({ id: user.id });
 });
 
