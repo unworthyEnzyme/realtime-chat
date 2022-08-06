86e805c982a5480cc3994022bb533c9a9b6691d2
unworthyEnzyme <unworthy_enzyme@protonmail.com>
2022-08-06 22:54:53 +0300

    hash the password

--- apps/server/src/auth/auth.router.ts
+++ apps/server/src/auth/auth.router.ts
@@ -1,3 +1,4 @@
+import argon2 from "argon2";
 import express from "express";
 import { z } from "zod";
 import prisma from "../prisma";
@@ -16,15 +17,34 @@ const router = express.Router();
 const signupSchema = z.object({
 	username: z.string(),
 	email: z.string().email(),
-	password: z.string(),
+	/**
+	 * Make this bigger in the production.
+	 * If you don't do this then `argon2.verify` will throw because the `plain`
+	 * argument is empty.
+	 */
+	password: z.string().min(1),
 });
 router.post("/signup", async (req, res) => {
-	const [data, err] = await safe(() => signupSchema.parseAsync(req.body));
-	if (err !== null) {
-		res.status(400).json({ error: "Invalid schema" });
+	const [data, parseError] = await safe(() =>
+		signupSchema.parseAsync(req.body)
+	);
+	if (parseError !== null) {
+		res.status(400).json({ error: parseError });
 		return;
 	}
-	const [user, err2] = await safe(() => prisma.user.create({ data }));
+	const hashedPassword = await argon2.hash(data.password, {
+		type: argon2.argon2id,
+		memoryCost: 2 ** 16,
+	});
+	const [user, err2] = await safe(() =>
+		prisma.user.create({
+			data: {
+				email: data.email,
+				username: data.username,
+				password: hashedPassword,
+			},
+		})
+	);
 	if (err2 !== null) {
 		res.status(400).json({ error: "The user already exists" });
 		return;
@@ -34,32 +54,37 @@ router.post("/signup", async (req, res) => {
 
 const loginSchema = z.object({
 	username: z.string(),
-	password: z.string(),
+	password: z.string().min(1 /**Make this bigger in the production */),
 });
 router.post("/login", async (req, res) => {
-	const [loginData, err] = await safe(() => loginSchema.parseAsync(req.body));
-	if (err !== null) {
-		res.status(400).json({ error: "Invalid schema" });
+	const [loginData, parseError] = await safe(() =>
+		loginSchema.parseAsync(req.body)
+	);
+	if (parseError !== null) {
+		res.status(400).json({ error: parseError });
 		return;
 	}
-	const [user, err2] = await safe(() =>
+	const [user, internalError] = await safe(() =>
 		prisma.user.findUnique({ where: { username: loginData.username } })
 	);
-	if (err2 !== null) {
+	if (internalError !== null) {
 		res.sendStatus(500);
 		return;
 	}
-	if (user === null) {
-		//TODO: Don't give away so much info.
-		res.status(400).json({ error: "User not found" });
+	const [verified, verificationError] = await safe(() =>
+		/**
+		 * Because the right side of the short circuit is an empty string is empty
+		 * it will throw that is why i use the `safe` function.
+		 */
+		argon2.verify(user?.password || "", loginData.password)
+	);
+	if (!verified || verificationError !== null) {
+		res.status(400).json({ error: "Username or password is incorrect" });
 		return;
 	}
-	if (user.password !== loginData.password) {
-		//TODO: Don't give away so much info.
-		res.status(400).json({ error: "Wrong password" });
-		return;
-	}
-	res.json({ data: "You are logged in." });
+	res.json({
+		data: user!.id /**If the password is verified then the user must exists */,
+	});
 });
 
 export default router;
