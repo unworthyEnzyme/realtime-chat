6b52dc7ca7d4db495590cb23b1dae487f100e2ba
unworthyEnzyme <unworthy_enzyme@protonmail.com>
2022-08-06 22:10:09 +0300

    not a secure login logic

--- apps/server/src/auth/auth.router.ts
+++ apps/server/src/auth/auth.router.ts
@@ -32,4 +32,34 @@ router.post("/signup", async (req, res) => {
 	res.json({ id: user.id });
 });
 
+const loginSchema = z.object({
+	username: z.string(),
+	password: z.string(),
+});
+router.post("/login", async (req, res) => {
+	const [loginData, err] = await safe(() => loginSchema.parseAsync(req.body));
+	if (err !== null) {
+		res.status(400).json({ error: "Invalid schema" });
+		return;
+	}
+	const [user, err2] = await safe(() =>
+		prisma.user.findUnique({ where: { username: loginData.username } })
+	);
+	if (err2 !== null) {
+		res.sendStatus(500);
+		return;
+	}
+	if (user === null) {
+		//TODO: Don't give away so much info.
+		res.status(400).json({ error: "User not found" });
+		return;
+	}
+	if (user.password !== loginData.password) {
+		//TODO: Don't give away so much info.
+		res.status(400).json({ error: "Wrong password" });
+		return;
+	}
+	res.json({ data: "You are logged in." });
+});
+
 export default router;
