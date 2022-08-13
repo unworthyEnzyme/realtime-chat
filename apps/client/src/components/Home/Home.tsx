26142ecf80167ba21fa0feb8eac4421d9da34266
unworthyEnzyme <unworthy_enzyme@protonmail.com>
2022-08-14 00:01:12 +0300

    i can receive message over a websocket

--- apps/client/src/components/Home/Home.tsx
+++ apps/client/src/components/Home/Home.tsx
@@ -1,5 +1,37 @@
+import { useEffect, useState } from "react";
+import { io, Socket } from "socket.io-client";
+
+interface Message {
+	id: string;
+	from: string;
+	content: string;
+}
 const Home = () => {
-	return <div>Home</div>;
+	const [socket, setSocket] = useState<Socket>();
+	const [messages, setMessages] = useState<Message[]>([]);
+	useEffect(() => {
+		const socketInstance = io("ws://localhost:8000", {
+			withCredentials: true,
+		});
+		setSocket(() => socketInstance);
+		return () => {
+			socketInstance.disconnect();
+		};
+	}, []);
+
+	useEffect(() => {
+		socket?.on("message", (message: Message) => {
+			setMessages((messages) => [message, ...messages]);
+			console.log(message);
+		});
+	}, [socket]);
+	return (
+		<div>
+			{messages.map((message: Message) => (
+				<div key={message.id}>{message.content}</div>
+			))}
+		</div>
+	);
 };
 
 export default Home;
