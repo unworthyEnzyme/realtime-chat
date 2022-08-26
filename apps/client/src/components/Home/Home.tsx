e333aeb26a22c31e34a6b02427e622e4d1a8123f
unworthyEnzyme <unworthy_enzyme@protonmail.com>
2022-08-26 22:27:59 +0300

    add video chat component.

--- apps/client/src/components/Home/Home.tsx
+++ apps/client/src/components/Home/Home.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import { DB } from "../../store/DB";
 import ChatFeed from "../ChatFeed/ChatFeed";
 import Chats from "../Chats/Chats";
+import VideoChat from "../VideoChat/Videochat";
 
 const Home = () => {
 	const db = new DB();
@@ -10,6 +11,7 @@ const Home = () => {
 		<Routes>
 			<Route path="/chat-with/:friend" element={<ChatFeed db={db} />} />
 			<Route path="/chats" element={<Chats db={db} />} />
+			<Route path="/video-chat" element={<VideoChat />} />
 		</Routes>
 	);
 };
---
+++ apps/client/src/components/VideoChat/Videochat.tsx
@@ -0,0 +1,5 @@
+const VideoChat = () => {
+	return <div>Video Chat</div>;
+};
+
+export default VideoChat;
